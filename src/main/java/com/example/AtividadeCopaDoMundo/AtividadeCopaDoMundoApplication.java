26fa4fd2e133560040370996b8e48574b09a70b4
gedianfunka <gediangabrielf@gmail.com>
2026-06-08 08:26:01 -0300

    OK

---
+++ src/main/java/com/example/AtividadeCopaDoMundo/AtividadeCopaDoMundoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AtividadeCopaDoMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeCopaDoMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeCopaDoMundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AtividadeCopaDoMundo/AtividadeCopaDoMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AtividadeCopaDoMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeCopaDoMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
